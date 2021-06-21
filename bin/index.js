254b25943450b5b0e0294690faa9b09500c87013
Yan-Jobs <yan@yanjobs.me>
2021-06-21 15:26:00 +0200

    :books: Add lib directory !

--- bin/index.js
+++ bin/index.js
@@ -1,31 +1,6 @@
 #!/usr/bin/env node
 
-import arg from "arg";
-import fs from "fs";
-
-fs.access("/.rpcrc.json", fs.F_OK, (err) => {
-  if (err) {
-    console.error(err);
-    fs.appendFile("/.rpcrc.json", "{\n}", function (err) {
-      if (err) throw err;
-      console.log("Fichier créé !");
-    });
-  }
-});
-function parseArgumentsIntoOptions(rawArgs) {
-  const args = arg(
-    {},
-    {
-      argv: rawArgs.slice(2)
-    }
-  );
-  return {
-    configName: args._[0] || null
-  };
-}
-function cli(args) {
-  const options = parseArgumentsIntoOptions(args);
-  console.log(options);
-}
-
-cli(process.argv);
+// load the cli
+import cli from "../lib/cli.js";
+// Start it
+cli(process);
